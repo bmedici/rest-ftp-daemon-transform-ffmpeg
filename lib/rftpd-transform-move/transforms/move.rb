5b78ead2dfa4d260fd8fdd6c6a08ef04e3b000d9
Bruno Medici <bm.git@bmconseil.com>
2017-06-27 11:05:44 +0200

    add code to handle file move

--- lib/rftpd-transform-move/transforms/move.rb
+++ lib/rftpd-transform-move/transforms/move.rb
@@ -1,5 +1,23 @@
 module RestFtpDaemon
   class TaskTransformMove < TaskTransform
 
+    def process
+      super
+
+      transform_each_unit
+    end
+
+  protected
+
+    def transform input, output
+      # Fake transformation
+      FileUtils.move input.path_abs, output.path_abs
+
+      log_debug "move results", {
+        input_size: input.size,
+        output_size: output.size,
+        }
+    end
+
   end
 end
